01af276bc9bc18b886ebb6b04b9f7b07ad2d2f75
sander fuhr <sandef@stud.ntnu.no>
2021-05-14 20:21:51 +0200

    Errorhandling implemented

--- api/CurrencyInfo.go
+++ api/CurrencyInfo.go
@@ -13,9 +13,15 @@ func CurrencyHandler(w http.ResponseWriter, r *http.Request) {
 
 	outputStruct := lib.Cryptos[(strings.ToUpper(currency))]
 
-	w.Header().Add("content-type", "application/json")
-	err := json.NewEncoder(w).Encode(outputStruct)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if outputStruct.Name != "" {
+
+		w.Header().Add("content-type", "application/json")
+		err := json.NewEncoder(w).Encode(outputStruct)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+	} else {
+		http.Error(w, "Error: "+currency+" not found." ,http.StatusBadRequest)
 	}
 }
