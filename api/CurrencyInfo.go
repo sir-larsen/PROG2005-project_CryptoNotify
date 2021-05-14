4119741ff746ffe84c6df9d2b1517c52879af540
sander fuhr <sandef@stud.ntnu.no>
2021-05-14 19:53:40 +0200

    Function declared. needs work on searching internal map

--- api/CurrencyInfo.go
+++ api/CurrencyInfo.go
@@ -1,2 +1,22 @@
 package api
 
+import (
+	lib "CryptoNotify/coreLib"
+	"encoding/json"
+	"fmt"
+	"github.com/go-chi/chi"
+	"net/http"
+)
+
+func CurrencyHandler(w http.ResponseWriter, r *http.Request) {
+	currency := chi.URLParam(r, "currency_code")
+	fmt.Print(currency)
+
+	outputStruct := lib.Cryptos[currency]
+
+	w.Header().Add("content-type", "application/json")
+	err := json.NewEncoder(w).Encode(outputStruct)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
