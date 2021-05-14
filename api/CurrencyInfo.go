2e2b745b01ca2269407b1040cd2fd12a00761ff8
sander fuhr <sandef@stud.ntnu.no>
2021-05-14 20:36:19 +0200

    Function works with both symbols and names now

--- api/CurrencyInfo.go
+++ api/CurrencyInfo.go
@@ -13,16 +13,36 @@ func CurrencyHandler(w http.ResponseWriter, r *http.Request) {
 
 	outputStruct := lib.Cryptos[(strings.ToUpper(currency))] //Locating data from internal database
 
-	if outputStruct.Name != "" {   //If the currency is found
+	if outputStruct.Name != "" { //If the currency is found
 
 		w.Header().Add("content-type", "application/json")
-		err := json.NewEncoder(w).Encode(outputStruct)   //Display in json-format
+		err := json.NewEncoder(w).Encode(outputStruct) //Display in json-format
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 
 	} else {
-		//Currency not found. Bad request
-		http.Error(w, "Error: "+currency+" not found." ,http.StatusBadRequest)
+		var currencyName string
+		for key, element := range lib.Cryptos {
+
+			if element.Name == currency {
+
+				currencyName = key
+			}
+		}
+		outputStruct2 := lib.Cryptos[(strings.ToUpper(currencyName))]
+
+		if outputStruct2.Name != "" {
+			w.Header().Add("content-type", "application/json")
+			err := json.NewEncoder(w).Encode(outputStruct2) //Display in json-format
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+		}else {
+			//Currency not found. Bad request
+			http.Error(w, "Error: "+currency+" not found.", http.StatusBadRequest)
+		}
+
 	}
 }
