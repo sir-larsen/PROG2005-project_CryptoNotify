d7df6a926d15250b899471e1e0d9cbe62f631813
sander fuhr <sandef@stud.ntnu.no>
2021-05-14 20:11:32 +0200

    Function working properly. Cleanup and errorhandling next

--- api/CurrencyInfo.go
+++ api/CurrencyInfo.go
@@ -3,16 +3,19 @@ package api
 import (
 	lib "CryptoNotify/coreLib"
 	"encoding/json"
-	"fmt"
 	"github.com/go-chi/chi"
 	"net/http"
+	"strings"
 )
 
 func CurrencyHandler(w http.ResponseWriter, r *http.Request) {
 	currency := chi.URLParam(r, "currency_code")
-	fmt.Print(currency)
+	capital := strings.ToUpper(currency)
 
-	outputStruct := lib.Cryptos[currency]
+	var outputStruct lib.CryptoInternal
+	outputStruct = lib.Cryptos[capital]
+
+
 
 	w.Header().Add("content-type", "application/json")
 	err := json.NewEncoder(w).Encode(outputStruct)
