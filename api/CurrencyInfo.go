4e24884d7d813c2c32ab34a9c81a29261f3bcf14
sander fuhr <sandef@stud.ntnu.no>
2021-05-15 21:27:35 +0200

    Cleanup

--- api/CurrencyInfo.go
+++ api/CurrencyInfo.go
@@ -3,7 +3,6 @@ package api
 import (
 	lib "CryptoNotify/coreLib"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strings"
 
@@ -13,17 +12,6 @@ import (
 func CurrencyHandler(w http.ResponseWriter, r *http.Request) {
 	currency := chi.URLParam(r, "currency_code")             //Currency symbol extracted from url
 	outputStruct := lib.Cryptos[(strings.ToUpper(currency))] //Locating data from internal database
-	//url := strings.Split(r.URL.String(), "/")
-	fmt.Println(currency)
-
-	if currency == "" {
-		w.Header().Add("content-type", "application/json")
-		err := json.NewEncoder(w).Encode( /*lib.CryptoResp*/ lib.Cryptos)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			fmt.Println("HHHHHHHH")
-		}
-	} else {
 
 		if outputStruct.Name != "" { //If the currency is found
 
@@ -57,8 +45,8 @@ func CurrencyHandler(w http.ResponseWriter, r *http.Request) {
 				http.Error(w, "Error: "+currency+" not found.", http.StatusBadRequest)
 			}
 		}
-	}
 }
+
 
 func AllCurrencies(w http.ResponseWriter, r *http.Request) {
 
