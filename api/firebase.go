005f3b721252ee67539e549b722b70ef2181b8af
Alexander <alexael@ntnu.no>
2021-05-15 17:17:03 +0200

    implemented delete function from firebase

--- api/firebase.go
+++ api/firebase.go
@@ -16,6 +16,7 @@ var Ctx context.Context
 var Client *firestore.Client
 var projectID = "cloud-project-dd1b4"
 
+//AddVolumeWebhook - Function for adding a volume webhook to the firebase collection
 func AddVolumeWebhook(webhook lib.VolumeWebhook, w http.ResponseWriter, r *http.Request) {
 
 	ref, _, err := Client.Collection(collectionVolume).Add(Ctx, map[string]interface{}{
@@ -38,21 +39,18 @@ func AddVolumeWebhook(webhook lib.VolumeWebhook, w http.ResponseWriter, r *http.
 	}
 }
 
-
-
 func AddPriceWebhook(webhook lib.PriceWebhook, w http.ResponseWriter, r *http.Request) {
 
 	ref, _, err := Client.Collection(collectionPrice).Add(Ctx, map[string]interface{}{
-		"CurrentPrice":		 webhook.CurrentPrice,
-		"HasTriggered":      webhook.HasTriggered,
-		"IsPriceIncrease":   webhook.IsPriceIncrease,
-		"Name":              webhook.Name,
-		"Number":            webhook.Number,
-		"StartPrice":        webhook.StartPrice,
-		"Symbol": 			 webhook.Symbol,
-		"TargetPrice":       webhook.TargetPrice,
-		"Url":               webhook.Url,
-
+		"CurrentPrice":    webhook.CurrentPrice,
+		"HasTriggered":    webhook.HasTriggered,
+		"IsPriceIncrease": webhook.IsPriceIncrease,
+		"Name":            webhook.Name,
+		"Number":          webhook.Number,
+		"StartPrice":      webhook.StartPrice,
+		"Symbol":          webhook.Symbol,
+		"TargetPrice":     webhook.TargetPrice,
+		"Url":             webhook.Url,
 	})
 	if err != nil {
 		http.Error(w, "Error when adding webhook "+webhook.Url, http.StatusBadRequest)
@@ -62,4 +60,12 @@ func AddPriceWebhook(webhook lib.PriceWebhook, w http.ResponseWriter, r *http.Re
 	}
 }
 
-
+//DeleteVolumeWebhook - for deleting a volume webhook from the webhooks_volume collection in firebase
+func DeleteVolumeWebhook(w http.ResponseWriter, r *http.Request, id string) {
+	_, err := Client.Collection(collectionVolume).Doc(id).Delete(Ctx)
+	if err != nil {
+		http.Error(w, "Deletion of "+id+" failed.", http.StatusInternalServerError)
+		return
+	}
+	http.Error(w, "Deletion of "+id+" successful if id existed, if else not nothing happened", http.StatusNoContent)
+}
