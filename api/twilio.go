68132f54bad7ddbc7c2b9bc07e3f265e0db6027e
Alexander <alexael@ntnu.no>
2021-05-15 19:15:00 +0200

    minor change in sms code

--- api/twilio.go
+++ api/twilio.go
@@ -7,17 +7,18 @@ import (
 	"strings"
 )
 
+var accountSid = "ACbe76999159d78a3662c88690bf3dbb8f" //Not optimal for security doing this
+var authToken = "a331462d3a8a090916adc7d055ca5323"
+var urlStr = "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
+var twilioNum = "+13157534147"
+
 func SendMessage() {
-	//Setting initial variables
-	accountSid := "ACbe76999159d78a3662c88690bf3dbb8f" //Not optimal for security doing this
-	authToken := "a331462d3a8a090916adc7d055ca5323"
-	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
 
 	// Build out the data for our message
 	v := url.Values{}
 	v.Set("To", "+4793044522")
-	v.Set("From", "+13157534147")
-	v.Set("Body", "Briefcase Mong!")
+	v.Set("From", twilioNum)
+	v.Set("Body", "Briefcase wanker")
 	rb := *strings.NewReader(v.Encode())
 
 	// Create client
